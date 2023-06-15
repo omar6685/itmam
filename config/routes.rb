b9fa85b016c32aca63724bc52a9cb392280b4acb
omar6685 <omr6685@hotmail.com>
2023-06-15 21:28:30 +0300

    add home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root "inspections#index"
+  get 'home_page/index'
+  root "home_page#index"
   resources :inspections
   resources :company_statuses
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
