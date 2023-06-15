e742fca6f38681b95da7e5ce5d9cda357d9e6388
omar6685 <omr6685@hotmail.com>
2023-06-15 20:43:00 +0300

    add root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "inspections#index"
   resources :inspections
   resources :company_statuses
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
