d157934a2e1b037aefc122ac1e8b3ceddf6d3bff
omar6685 <omr6685@hotmail.com>
2023-06-17 14:34:39 +0300

    done from home

---
+++ app/javascript/itmam.js
@@ -0,0 +1,17 @@
+
+window.addEventListener("load", function () {
+    var logo = document.getElementById("logo");
+    var deco = document.getElementById("deco");
+    var text = document.getElementById("text");
+
+    // Animate the logo
+    logo.style.width = "106px";
+    logo.style.opacity = "1";
+
+    // Animate the deco
+    deco.style.transform = "translateY(0)";
+
+    // Animate the button
+    text.style.opacity = "1"
+
+});
