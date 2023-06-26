32b861e054e2d17636bda80dbc0ea76d2e154e28
omar6685 <omr6685@hotmail.com>
2023-06-26 17:03:44 +0300

    add company size

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_26_120409) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_26_125043) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "activity_tables", force: :cascade do |t|
     t.string "activitiy"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "company_sizes", force: :cascade do |t|
+    t.string "size"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
